b66d7abd491d5d876a7d1294309d726cb7e32e77
Taishi Naritomi <79634025+taishinaritomi@users.noreply.github.com>
2023-08-13 16:02:01 +0900

    fix: change the out dir for wasm (#10)

---
+++ .changeset/odd-mugs-repair.md
@@ -0,0 +1,5 @@
+---
+"excss": patch
+---
+
+fix: change the out dir for wasm
--- packages/excss/src/compiler/compiler.ts
+++ packages/excss/src/compiler/compiler.ts
@@ -2,4 +2,4 @@ import { webcrypto } from "node:crypto";
 globalThis.crypto = webcrypto;
 
 export type Variants = Record<string, string | number>;
-export { transform } from "../../binding/compiler_wasm/excss_compiler_wasm";
+export { transform } from "../../dist/_wasm/compiler/excss_compiler_wasm";
