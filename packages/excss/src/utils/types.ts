e78fab46b962a5da39bf39601e491244894aaf0f
taishinaritomi <taishinaritomi@gmail.com>
2023-08-16 23:41:38 +0900

    feat: strict the types for the ex API and add tests.

--- packages/excss/src/utils/types.ts
+++ packages/excss/src/utils/types.ts
@@ -8,3 +8,20 @@ export type ToLiteral<T, L extends Literal> = T extends `${infer R extends L}`
 export type Pretty<T> = { [P in keyof T]: T[P] } & {};
 
 export type If<Q extends boolean, T, F> = Q extends true ? T : F;
+
+type TSRequired<T> = { [P in keyof T]-?: T[P] };
+type ExcludeValue<T, V> = { [P in keyof T]: Exclude<T[P], V> };
+type ExtendValue<T, V> = { [P in keyof T]: T[P] | V };
+
+type StdRequired<T> = ExcludeValue<TSRequired<T>, undefined>;
+type StdOptional<T> = ExtendValue<Partial<T>, undefined>;
+
+export type Required<T, Optional extends keyof T = never> = Pretty<
+  StdOptional<Pick<T, Optional>> &
+    StdRequired<Pick<T, Exclude<keyof T, Optional>>>
+>;
+
+export type Optional<T, Required extends keyof T = never> = Pretty<
+  StdRequired<Pick<T, Required>> &
+    StdOptional<Pick<T, Exclude<keyof T, Required>>>
+>;
