eecba1fca5f3c1597633330b2a92b77c0610a5b8
taishinaritomi <taishinaritomi@gmail.com>
2023-09-07 22:54:53 +0900

    simplify next plugin

--- packages/excss/src/plugins/next.ts
+++ packages/excss/src/plugins/next.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
-import { lazyPostCSS } from "next/dist/build/webpack/config/blocks/css/index.js";
-import { getGlobalCssLoader } from "next/dist/build/webpack/config/blocks/css/loaders/index.js";
-import type { ConfigurationContext } from "next/dist/build/webpack/config/utils.js";
-import type { Configuration, RuleSetRule } from "webpack";
-import type { ExcssOption } from "./webpack/plugin.ts";
+import type { Configuration } from "webpack";
 import ExcssPlugin from "./webpack/plugin.ts";
 
 type ExcssConfig = {
@@ -15,50 +11,13 @@ type ExcssConfig = {
 
 function plugin(nextConfig: NextConfig, excssConfig: ExcssConfig) {
   return {
-    webpack(config: Configuration & ConfigurationContext, options) {
-      const { dir, dev, isServer, config: resolvedNextConfig } = options;
-      const cssRules = (
-        config.module?.rules?.find((rule) => {
-          return rule
-            ? typeof rule === "object" &&
-                Array.isArray(rule.oneOf) &&
-                rule.oneOf.some((oneOfRule) => {
-                  return oneOfRule
-                    ? oneOfRule.test instanceof RegExp &&
-                        typeof oneOfRule.test.test === "function" &&
-                        oneOfRule.test.test("filename.css")
-                    : false;
-                })
-            : false;
-        }) as RuleSetRule
-      ).oneOf;
-
-      cssRules?.unshift({
-        test: /.css$/,
-        sideEffects: true,
-        use: getGlobalCssLoader(
-          {
-            assetPrefix: config.assetPrefix,
-            isClient: !isServer,
-            isServer,
-            isDevelopment: dev,
-            future: (resolvedNextConfig["future"] as unknown) || {},
-            experimental: resolvedNextConfig.experimental,
-            hasAppDir: resolvedNextConfig.experimental.appDir,
-          } as ConfigurationContext,
-          () => lazyPostCSS(dir, undefined, undefined),
-          [],
-        ),
-      });
-
-      const isAppDir = resolvedNextConfig.experimental.appDir ?? true;
-
-      const excssWebpackPluginOption: ExcssOption = {
-        cssOutDir: isAppDir ? "./.next/cache/excss" : undefined,
-        inject: excssConfig.inject,
-      };
-
-      config.plugins?.push(new ExcssPlugin(excssWebpackPluginOption));
+    webpack(config: Configuration, options) {
+      config.plugins?.push(
+        new ExcssPlugin({
+          cssOutDir: "./.next/cache/excss",
+          inject: excssConfig.inject,
+        }),
+      );
 
       if (typeof nextConfig.webpack === "function") {
         return nextConfig.webpack(config, options) as unknown;
